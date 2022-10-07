0ecab1e2004f8f797a644cacfc19b68e7259f612
Nils Martel <nilsmartel@yahoo.de>
2022-10-07 14:50:33 +0200

    Create CLI to pick random elements

---
+++ README.md
@@ -0,0 +1,10 @@
+# random-element
+
+cli to easily pick random elements
+
+    Usage:
+        random-element <...elements>
+
+        or
+
+        cat ./lines.txt | random-element
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::f64::consts::E;
+
+fn print_help() -> ! {
+    println!(
+        "Usage:
+    random-element <...elements>
+
+    or
+
+    cat ./lines.txt | random-element
+"
+    );
+    std::process::exit(1);
+}
+fn main() {
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
+    if args.contains(&String::from("--help")) {
+        print_help();
+    }
+
+    let elements = if args.len() > 0 {
+        args
+    } else {
+        // stdin mode activated
+        use std::io::*;
+        BufReader::new(stdin())
+            .lines()
+            .collect::<Result<_>>()
+            .expect("read lines from stdin")
+    };
+
+    let index = rand::random::<f64>() * elements.len() as f64;
+
+    let element = &elements[index.floor() as usize];
+
+    print!("{element}");
+}
